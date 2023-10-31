ecd297ded41e141715b50ca63e3d628ba7b11304
swagvillain <marshall.taylor.brown@gmail.com>
2023-10-30 20:53:10 -0700

    finished product. ready to submit

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,55 @@
 public class Main {
   public static void main(String[] args) {
-    int [] testArr1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+    int [] testArr1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23};
     int [] testArr2 = {5, 86, 124, 9, 5489, 3524, 7849, 356, 1245, 8756, 3501, 3512, 7878, 7979};
     int [] testArr3 = {12, 2, 5, 4, 13};
 
-    //test(testArr1);
-    test(testArr2);
-    //test(testArr3);
+    boolean test1Results = test(testArr1);
+    boolean test2Results = test(testArr2);
+    boolean test3Results = test(testArr3);
 
+    if(test1Results && test2Results && test3Results){
+      System.out.println("all tests successful");
+    }else {
+      if (test1Results) {
+        System.out.println("test 1 successful.");
+      } else{
+        System.out.println("test 1 unsuccessful.");
+      }
+      if (test2Results) {
+        System.out.println("test 2 successful.");
+      } else{
+        System.out.println("test 2 unsuccessful.");
+      }
+      if (test3Results) {
+        System.out.println("test 3 successful.");
+      } else{
+        System.out.println("test 3 unsuccessful.");
+      }
+    }
   }
   
-  public static void test(int[] testArr){
-      MyCuckooHash myHash = new MyCuckooHash(testArr.length+2);
-      
-      for(int i = 0; i < testArr.length; i++){
-        myHash.add(testArr[i]);
-      }
+  public static boolean test(int[] testArr){
+    MyCuckooHash myHash = new MyCuckooHash(testArr.length+2);
+    boolean allGood = true;
 
-      System.out.print("final result: ");
-      myHash.printHash();
-      myHash.printRehashCounter();
-      myHash.printEvictionCounter();
-      System.out.println("------------------------------------------------------------");
+    for(int i = 0; i < testArr.length; i++){
+      myHash.add(testArr[i]);
+    }
+    System.out.print("final result: ");
+    myHash.printHash();
 
+    for(int i = 0; i < testArr.length; i++){ //this checks that each key is found in the hashmap
+      int k = myHash.fetch(testArr[i]);
+      //System.out.println(testArr[i] + " = " + k); debugging, verifying that keys are found at addresses that they should be hashed to
+      if(testArr[i] != k)
+        allGood = false;
+    }
+
+    myHash.printRehashCounter();
+    myHash.printEvictionCounter();
+    System.out.println("------------------------------------------------------------");
+
+    return allGood;
   }
 }
--- src/MyCuckooHash.java
+++ src/MyCuckooHash.java
@@ -6,70 +6,77 @@ public class MyCuckooHash {
   }
 
   // non-static variables
-  int i = 0;
-  int decimalIndex1 = 0;
-  int decimalIndex2 = 1;
-  double currentDecimal1 = decimalsArr[decimalIndex1];
-  double currentDecimal2 = decimalsArr[decimalIndex2];
-  int[] arr;
+  private int i = 0;
+  private int decimalIndex1 = 0;
+  private int decimalIndex2 = 1;
+  private double currentDecimal1 = decimalsArr[decimalIndex1];
+  private double currentDecimal2 = decimalsArr[decimalIndex2];
+  private int[] arr;
   private static double[] decimalsArr = { 0.3332, 0.4589, 0.121212, 0.9845, 0.4654, 0.08, 0.9, 0.8742, 
     0.66, 0.25 };
-  int evictionStart;
-  int rehashCounter = 0;
-  int evictionCounter= 0;
+  private int evictionStart;
+  private int rehashCounter = 0; //tracks how many times hash functions are renewed
+  private int evictionCounter= 0; //tracks how many times evictions occur
 
-  // functions here
+  // functions below
 
+  // prints number of times hash functions have been renewed
   public void printRehashCounter(){
     System.out.println("Number of rehashes: " + rehashCounter);
   }
 
+  // prints number of times evictions have occured
   public void printEvictionCounter(){
         System.out.println("Number of evictions: " + evictionCounter);
-
   }
 
+  //adds key to the hashtable
   public void add(int x) {
     int address1 = hashFn1(x); // hash the key
     int address2 = hashFn2(x); // hash the key again
 
-    // legacy debugging help
-    //printHash();
     System.out.println("key to be inserted is " + x + ". Its options are index " + address1 + " and index " + address2 + ".");
 
-    if (arr[address1] == 0) {
+    if (arr[address1] == 0) { // if first address is empty, the key will be assigned there
       arr[address1] = x;
       System.out.println("inserting at "+ address1);
-    } else if (arr[address2] == 0) {
+    } else if (arr[address2] == 0) { // if first option is full, second is used if free
       arr[address2] = x;
       System.out.println(address1 + " is taken. inserting at " + address2);
-    } else {
+    } else { // if both are full, we evict the first and take its place.
       System.out.println("both are full. evicting " + arr[address1] + " from " + address1);
-      evictionStart = x;
-      evict(address1, x);
-      arr[address1] = x;
-    }      
+      evictionStart = x; // noting where we are when we begin evictions, to prevent infinite loop
+      boolean reHashed = evict(address1, x); // evicting value at first address.
+      if(reHashed) // if eviction above resulted in a loop, and we rehashed the table after refreshing the functions,
+        arr[address1] = x; // then we need to remember to assign this value where it belongs.
+    }
     System.out.println("Result:");
     printHash();
   }
-
-  private void evict(int address, int x) {
+  /**
+   * @param address,
+   * @param x,
+   * @return boolean, if eviction resulted in a rehashing
+   */
+  private boolean evict(int address, int x) {
+    boolean reHashed = false;
     evictionCounter++;
     int y = arr[address];
-    if(y == evictionStart) {
-      reHash(); //then what
+    if(y == evictionStart) { //checking if this is a return to a beginning of an infinite loop
+      reHash(x); //if is a loop, then get new hash functions and reset.
+      reHashed = true;
     } else {
       arr[address] = x;
-      // need to insert y where it belongs, but NOT at address 1!
       printHash();
       reHome(address, y);
     }
+    return reHashed;
   }
 
   private void reHome(int oldAddress, int y) {
     int newAddress;
 
-    if (hashFn1(y) == oldAddress) {
+    if (hashFn1(y) == oldAddress) { //determining which of the 2 hashfunctions to use
       newAddress = hashFn2(y);
     } else {
       newAddress = hashFn1(y);
@@ -99,15 +106,14 @@ public class MyCuckooHash {
    */
   public int getAddress(int x) {
     if(arr[hashFn1(x)] == x){
-      return x;
+      return hashFn1(x);
+    } else if (arr[hashFn2(x)] == x) {
+        return hashFn2(x);
     }
-      else if (arr[hashFn2(x)] == x){
-        return x;
-      }
-      return -1;
+      return 4;
   }
 
-  public boolean isOccupied(int x) {
+  private boolean isOccupied(int x) {
     for (int i = 0; i < arr.length; i++) {
       if (arr[i] == x) {
         return true;
@@ -116,13 +122,20 @@ public class MyCuckooHash {
     return false;
   }
 
-  public void remove() {
-
+  public void remove(int x) {
+    int address = getAddress(x);
+    arr[address] = 0;
   }
 
-  private void reHash(){
+  public int fetch (int x){
+    int address = getAddress(x);
+    return arr[address];
+  }
+
+  private void reHash(int x){
     rehashCounter++;
 
+    // this was to help with debugging
     System.out.println("reHash called. About to rehash! Old decimals are: " + currentDecimal1 + " at " + decimalIndex1 + ", and " + currentDecimal2 + " at " + decimalIndex2);
     refreshHashFx();
     System.out.println("New decimals are: " + currentDecimal1 + " at " + decimalIndex1 + ", and " + currentDecimal2 + " at " + decimalIndex2);
@@ -135,11 +148,12 @@ public class MyCuckooHash {
         add(oldArr[i]);
       }
     }
+    add(x);
     System.out.println("done rehash, resuming where we were. results of rehash: ");
     printHash();
   }
   
-  private void refreshHashFx() {
+  private void refreshHashFx() { // systematically cycles through decimal coefficients used in hash functions
     System.out.println("refreshing hash functions");
     if (decimalIndex1 == 6)
       decimalIndex1++;
@@ -157,6 +171,7 @@ public class MyCuckooHash {
 
   }
 
+  // functions used to hash keys
   private int hashFn1(int x) {
     double address1;
     address1 = Math.floor((((currentDecimal1) * x)%1)*arr.length);
